c0cabda5b7aac72eb798ef958f303ce5c7ed18d3
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-09-05 01:23:29 +0300

    Fix second point in issue #1

--- app/src/main/java/com/rcl/mobilest/components/settings/SettingsComponent.kt
+++ app/src/main/java/com/rcl/mobilest/components/settings/SettingsComponent.kt
@@ -1,15 +1,25 @@
 package com.rcl.mobilest.components.settings
 
+import androidx.annotation.StringRes
 import com.arkivanov.decompose.ComponentContext
+import com.rcl.mobilest.R
 import com.rcl.mobilest.di.config.ConfigManager
+import com.rcl.mobilest.di.theme.ThemeManager
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
 class SettingsComponent(componentContext: ComponentContext) : ComponentContext by componentContext,
     KoinComponent {
     val configManager: ConfigManager by inject()
+    val themeManager: ThemeManager by inject()
 
     fun updateState(isDownload: Boolean, newValue: Boolean) {
         configManager.setToggle(isDown = isDownload, newValue = newValue)
+    }
+
+    enum class DarkThemeEnum(val value: Boolean?, @StringRes val id: Int) {
+        Dark(true, R.string.dark_theme),
+        Light(false, R.string.light_theme),
+        System(null, R.string.default_theme)
     }
 }
--- app/src/main/java/com/rcl/mobilest/components/settings/SettingsComponentImpl.kt
+++ app/src/main/java/com/rcl/mobilest/components/settings/SettingsComponentImpl.kt
@@ -5,9 +5,12 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,13 +21,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.rcl.mobilest.R
+import com.rcl.mobilest.components.settings.SettingsComponent.DarkThemeEnum
 
 @Composable
 fun SettingsComponentImpl(settingsComponent: SettingsComponent) {
     val isUpstreamEnabled by settingsComponent.configManager.isUpstreamEnabled.collectAsState()
     val isDownstreamEnabled by settingsComponent.configManager.isDownstreamEnabled.collectAsState()
+    val themeManager = settingsComponent.themeManager
+    val isDarkTheme by themeManager.isDarkTheme.collectAsState()
 
-    Column (
+    Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(16.dp),
@@ -33,13 +39,70 @@ fun SettingsComponentImpl(settingsComponent: SettingsComponent) {
         AvailabilityCard(
             textId = R.string.upstream,
             state = isUpstreamEnabled,
-            changeState = { settingsComponent.updateState(isDownload = false, newValue = !isUpstreamEnabled) }
+            changeState = {
+                settingsComponent.updateState(
+                    isDownload = false,
+                    newValue = !isUpstreamEnabled
+                )
+            }
         )
 
         AvailabilityCard(
             textId = R.string.downstream,
             state = isDownstreamEnabled,
-            changeState = { settingsComponent.updateState(isDownload = true, newValue = !isDownstreamEnabled) }
+            changeState = {
+                settingsComponent.updateState(
+                    isDownload = true,
+                    newValue = !isDownstreamEnabled
+                )
+            }
+        )
+
+        Spacer(modifier = Modifier.padding(10.dp))
+
+        Text(stringResource(R.string.theme_title))
+
+        Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+            DarkThemeEnum.entries.forEach { state ->
+                val selected = state.value == isDarkTheme
+                SelectableCard(
+                    selected = selected,
+                    value = state.value,
+                    onClick = {
+                        themeManager.setDarkThemeValue(it)
+                    },
+                    text = state.id
+                )
+            }
+        }
+    }
+}
+
+@Composable
+private fun SelectableCard(
+    selected: Boolean,
+    value: Boolean?,
+    onClick: (Boolean?) -> Unit,
+    @StringRes text: Int,
+    modifier: Modifier = Modifier
+) {
+    val cardColor = if (selected) {
+        CardDefaults.cardColors().copy(
+            containerColor = MaterialTheme.colorScheme.primary,
+            contentColor = MaterialTheme.colorScheme.onPrimary
+        )
+    } else {
+        CardDefaults.cardColors()
+    }
+    Card(
+        colors = cardColor,
+        modifier = modifier.clickable { onClick(value) }
+    ) {
+        Text(
+            text = stringResource(text),
+            modifier = Modifier
+                .padding(10.dp)
+                .align(Alignment.CenterHorizontally)
         )
     }
 }
