cfc5816744e51a329ce0d56b69647b8406440d77
RecodeLiner <58636614+RecodeLiner@users.noreply.github.com>
2024-09-05 13:46:43 +0300

    Fix point 1 in issue #1

--- app/src/main/java/com/rcl/mobilest/components/home/HomeComponent.kt
+++ app/src/main/java/com/rcl/mobilest/components/home/HomeComponent.kt
@@ -3,7 +3,11 @@ package com.rcl.mobilest.components.home
 import android.net.ConnectivityManager
 import com.arkivanov.decompose.ComponentContext
 import com.rcl.mobilest.di.config.ConfigManager
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
@@ -12,18 +16,32 @@ class HomeComponent(componentContext: ComponentContext) : ComponentContext by co
     KoinComponent {
     val configManager: ConfigManager by inject()
     val connectivityManager: ConnectivityManager by inject()
+    val connectionState = MutableStateFlow<NetworkResult>(NetworkResult.Success(0, 0))
     val currentState = MutableStateFlow<State>(State.Idle)
 
-    //TechTask doesn't included this, but id needed, it's possible to update state from main screen
-    fun updateState(isDownload: Boolean, newValue: Boolean) {
-        configManager.setToggle(isDown = isDownload, newValue = newValue)
+    private var downSpeedSum: Long = 0
+    private var upSpeedSum: Long = 0
+    private var measurementCount: Int = 0
+
+    fun startMeasurement() {
+        downSpeedSum = 0
+        upSpeedSum = 0
+        measurementCount = 0
+        currentState.value = State.Loading(0f)
+        CoroutineScope(Dispatchers.Main.immediate).launch {
+            repeat(60) {
+                currentState.value = State.Loading(((it/60*100).toFloat()))
+                checkConnectivity()
+                delay(1000)
+            }
+            val averageDownSpeed = if (measurementCount > 0) downSpeedSum / measurementCount else null
+            val averageUpSpeed = if (measurementCount > 0) upSpeedSum / measurementCount else null
+            connectionState.value = NetworkResult.Success(averageDownSpeed, averageUpSpeed)
+            currentState.value = State.Result
+        }
     }
 
-    fun checkConnectivity(
-        times: Int = 60,
-    ) {
-        currentState.value = State.Loading
-
+    private fun checkConnectivity() {
         val nc = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
         val result = if (nc == null) {
             NetworkResult.Failure
@@ -31,33 +49,37 @@ class HomeComponent(componentContext: ComponentContext) : ComponentContext by co
             NetworkResult.Success(
                 downSpeed =
                 if (configManager.isDownstreamEnabled.value)
-                    nc.linkDownstreamBandwidthKbps
+                    nc.linkDownstreamBandwidthKbps.toLong()
                 else
                     null,
                 upSpeed =
                 if (configManager.isUpstreamEnabled.value)
-                    nc.linkUpstreamBandwidthKbps
+                    nc.linkUpstreamBandwidthKbps.toLong()
                 else
                     null
             )
         }
 
-        if (result is NetworkResult.Failure) {
+        if (result is NetworkResult.Success) {
+            result.downSpeed?.let { downSpeedSum += it }
+            result.upSpeed?.let { upSpeedSum += it }
+            measurementCount++
+            currentState.value = State.Loading(measurementCount.toFloat() / 60)
+            connectionState.value = result
+        } else {
             currentState.value = State.Error
-        } else if (result is NetworkResult.Success) {
-            currentState.value = State.Result(result)
         }
     }
 
     sealed class NetworkResult {
-        data class Success(val downSpeed: Int?, val upSpeed: Int?) : NetworkResult()
+        data class Success(val downSpeed: Long?, val upSpeed: Long?) : NetworkResult()
         data object Failure : NetworkResult()
     }
 
     sealed class State {
         data object Idle : State()
-        data object Loading : State()
-        data class Result(val res: NetworkResult.Success) : State()
+        data class Loading(val progress: Float) : State()
+        data object Result : State()
         data object Error : State()
     }
 }
--- app/src/main/java/com/rcl/mobilest/components/home/HomeComponentImpl.kt
+++ app/src/main/java/com/rcl/mobilest/components/home/HomeComponentImpl.kt
@@ -1,8 +1,9 @@
 package com.rcl.mobilest.components.home
 
 import androidx.annotation.StringRes
-import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
@@ -20,39 +21,78 @@ import com.rcl.mobilest.R
 @Composable
 fun HomeComponentImpl(homeComponent: HomeComponent) {
     val state by homeComponent.currentState.collectAsState()
+    val connection by homeComponent.connectionState.collectAsState()
 
-    AnimatedContent(state, label = "") {
-        when (it) {
-            is HomeComponent.State.Idle -> {
-                RestartButton(text = R.string.start_measure, checkConnectivity = {
-                    homeComponent.checkConnectivity()
+    when (state) {
+        is HomeComponent.State.Idle -> {
+            RestartButton(text = R.string.start_measure, checkConnectivity = {
+                homeComponent.startMeasurement()
+            })
+        }
+
+        is HomeComponent.State.Result -> {
+            Column {
+                Text(
+                    if ((connection as HomeComponent.NetworkResult.Success).downSpeed != null ) {
+                        stringResource(
+                            id = R.string.down_speed_average_res,
+                            (connection as HomeComponent.NetworkResult.Success).downSpeed?.div(
+                                1024
+                            ).toString()
+                        )
+                    } else {
+                        stringResource(
+                            id = R.string.unavailable,
+                            stringResource(R.string.downstream)
+                        )
+                    }
+                )
+                Text(
+                    if ((connection as HomeComponent.NetworkResult.Success).upSpeed != null) {
+                        stringResource(
+                            id = R.string.up_speed_average_res,
+                            (connection as HomeComponent.NetworkResult.Success).upSpeed?.div(
+                                1024
+                            ).toString()
+                        )
+                    } else {
+                        stringResource(
+                            id = R.string.unavailable,
+                            stringResource(R.string.upstream)
+                        )
+                    }
+                )
+
+                RestartButton(text = R.string.restart, checkConnectivity = {
+                    homeComponent.startMeasurement()
                 })
             }
+        }
 
-            is HomeComponent.State.Result -> {
+        is HomeComponent.State.Error -> {
+            Column {
+                Text(
+                    text = stringResource(R.string.error)
+                )
+
+                RestartButton(text = R.string.restart, checkConnectivity = {
+                    homeComponent.startMeasurement()
+                })
+            }
+        }
+
+        is HomeComponent.State.Loading -> {
+            Row(horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                CircularProgressIndicator(
+                    progress = { (state as HomeComponent.State.Loading).progress }
+                )
+
                 Column {
                     Text(
-                        if (it.res.upSpeed != null) {
-                            stringResource(
-                                id = R.string.up_speed_res,
-                                it.res.upSpeed.div(
-                                    1024
-                                ).toString()
-                            )
-                        } else {
-                            stringResource(
-                                id = R.string.unavailable,
-                                stringResource(R.string.upstream)
-                            )
-                        }
-                    )
-                    Text(
-                        if (it.res.downSpeed != null ) {
+                        if ((connection as HomeComponent.NetworkResult.Success).downSpeed != null) {
                             stringResource(
                                 id = R.string.down_speed_res,
-                                it.res.downSpeed.div(
-                                    1024
-                                ).toString()
+                                ((connection as HomeComponent.NetworkResult.Success).downSpeed!!/1024).toString()
                             )
                         } else {
                             stringResource(
@@ -61,27 +101,20 @@ fun HomeComponentImpl(homeComponent: HomeComponent) {
                             )
                         }
                     )
-
-                    RestartButton(text = R.string.restart, checkConnectivity = {
-                        homeComponent.checkConnectivity()
-                    })
-                }
-            }
-
-            is HomeComponent.State.Error -> {
-                Column {
                     Text(
-                        text = stringResource(R.string.error)
+                        if ((connection as HomeComponent.NetworkResult.Success).upSpeed != null) {
+                            stringResource(
+                                id = R.string.up_speed_res,
+                                ((connection as HomeComponent.NetworkResult.Success).upSpeed!!/1024).toString()
+                            )
+                        } else {
+                            stringResource(
+                                id = R.string.unavailable,
+                                stringResource(R.string.upstream)
+                            )
+                        }
                     )
-
-                    RestartButton(text = R.string.restart, checkConnectivity = {
-                        homeComponent.checkConnectivity()
-                    })
                 }
-            }
-
-            is HomeComponent.State.Loading -> {
-                CircularProgressIndicator()
             }
         }
     }
